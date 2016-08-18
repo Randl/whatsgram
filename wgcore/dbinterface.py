b7914ae88b3c71a0ce1019db349d1d8a9d39371f
Evgeniy Zheltonozhskiy <zheltonozhskiy@gmail.com>
2016-08-19 02:29:50 +0300

    Added logging, removed commit from reading

--- wgcore/dbinterface.py
+++ wgcore/dbinterface.py
@@ -1,10 +1,13 @@
+import logging
 import sqlite3
 
+logger = logging.getLogger(__name__)
 
 class DBInterface:
     def __init__(self):
         self.db_filename = 'user_data.db'
         self.connection = sqlite3.connect(self.db_filename)
+        logger.info('Connected to database {}'.format(self.db_filename))
         self.cursor = self.connection.cursor()
         self.cursor.execute('CREATE TABLE IF NOT EXISTS users (user TEXT UNIQUE, whatsapp_id TEXT UNIQUE)')
         self.cursor.execute('CREATE TABLE IF NOT EXISTS conversations (id TEXT UNIQUE, user TEXT, participant TEXT)')
@@ -15,39 +18,64 @@ class DBInterface:
 
     def add_user(self, user_id, whatsapp_id):
         self.connection = sqlite3.connect(self.db_filename)
+        logger.info('Connected to database {}'.format(self.db_filename))
+        
         self.cursor = self.connection.cursor()
-        self.cursor.execute("INSERT INTO users VALUES (:user, :whatsapp)", {"user": user_id, "whatsapp": whatsapp_id})
+        self.cursor.execute('INSERT INTO users VALUES (:user, :whatsapp)', {'user': user_id, 'whatsapp': whatsapp_id})
+        
         self.connection.commit()
+        logger.info('Commiting changes to database {}. User {} added'.format(self.db_filename, user_id))
         self.connection.close()
 
     def add_conversation(self, user_id, participant):
         self.connection = sqlite3.connect(self.db_filename)
+        logger.info('Connected to database {}'.format(self.db_filename))
+        
         self.cursor = self.connection.cursor()
-        self.cursor.execute("INSERT INTO conversations VALUES (:user, :part)", {"user": user_id, "part": participant})
+        self.cursor.execute('INSERT INTO conversations VALUES (:user, :part)', {'user': user_id, 'part': participant})
+        
         self.connection.commit()
+        logger.info('Commiting changes to database {}. Conversation {} added'.format(self.db_filename, user_id))
         self.connection.close()
 
     def add_message(self, conversation_id, message):
-        return 0
+        self.connection = sqlite3.connect(self.db_filename)
+        logger.info('Connected to database {}'.format(self.db_filename))
+
+        self.cursor = self.connection.cursor()
+        # TODO
+
+        self.connection.commit()
+        # logger.info('Commiting changes to database {}. Message {} added'.format(self.db_filename, message))
+        self.connection.close()
 
     def get_conversations(self, user_id):
         self.connection = sqlite3.connect(self.db_filename)
+        logger.info('Connected to database {}'.format(self.db_filename))
+        
         self.cursor = self.connection.cursor()
-        self.cursor.execute("SELECT id, participant FROM conversations WHERE user = :user", {"user": user_id})
+        self.cursor.execute('SELECT id, participant FROM conversations WHERE user = :user', {'user': user_id})
         convs = self.cursor.fetchall()
-        self.connection.commit()
         self.connection.close()
         return convs
 
     def get_conversation(self, user_id, participant):
         self.connection = sqlite3.connect(self.db_filename)
+        logger.info('Connected to database {}'.format(self.db_filename))
+        
         self.cursor = self.connection.cursor()
-        self.cursor.execute("SELECT id, participant FROM conversations WHERE user = :user AND participant = :part",
-                            {"user": user_id, 'part': participant})
+        self.cursor.execute('SELECT id, participant FROM conversations WHERE user = :user AND participant = :part',
+                            {'user': user_id, 'part': participant})
         conv = self.cursor.fetchone()
-        self.connection.commit()
         self.connection.close()
         return conv
 
     def get_messages(self, conversation_id):
-        return 0
+        self.connection = sqlite3.connect(self.db_filename)
+        logger.info('Connected to database {}'.format(self.db_filename))
+
+        self.cursor = self.connection.cursor()
+        # TODO
+        messages = self.cursor.fetchall()
+        self.connection.close()
+        return messages
